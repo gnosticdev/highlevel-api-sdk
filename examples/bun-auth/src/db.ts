013f8213b2437767e4bab2b8c0bcb2f736eae98c
gnostic <64601257+nynxa@users.noreply.github.com>
2024-10-01 16:15:04 -0400

    chore: separate examples

---
+++ examples/bun-auth/src/db.ts
@@ -0,0 +1,87 @@
+import type { Database } from 'bun:sqlite'
+import type { AccessTokenResponse } from '@gnosticdev/highlevel-sdk/types'
+
+type DBColumns = {
+	id?: number
+	expiresAt: number
+} & Required<
+	Pick<
+		AccessTokenResponse,
+		'access_token' | 'refresh_token' | 'locationId' | 'userId'
+	>
+>
+
+type $Keys<T> = `$${T extends string ? T : never}`
+
+type InsertDBRow = Omit<
+	{
+		[K in keyof DBColumns as $Keys<K>]: DBColumns[K]
+	},
+	'$id'
+>
+
+/**
+ * Get the access token from the database
+ * @param db - The database instance
+ * @returns - The access token
+ */
+const getAccessToken = (sqlite: Database) => {
+	const token = sqlite.query<DBColumns, []>('SELECT * FROM tokens_table').get()
+	return token
+}
+
+/**
+ * Save a token response to the database
+ * @param db - The database instance
+ * @param dbRow - The token response to save
+ */
+const saveTokenResponse = (sqlite: Database, dbRow: DBColumns) => {
+	sqlite
+		.prepare<DBColumns, InsertDBRow>(
+			`INSERT INTO tokens_table (userId, locationId, access_token, refresh_token, expiresAt)
+            VALUES ($userId, $locationId, $access_token, $refresh_token, $expiresAt)
+            ON CONFLICT(userId) DO UPDATE SET
+            locationId = excluded.locationId,
+            access_token = excluded.access_token,
+            refresh_token = excluded.refresh_token,
+            expiresAt = excluded.expiresAt;
+            `,
+		)
+		.run({
+			$userId: dbRow.userId,
+			$locationId: dbRow.locationId,
+			$access_token: dbRow.access_token,
+			$expiresAt: dbRow.expiresAt,
+			$refresh_token: dbRow.refresh_token,
+		})
+}
+
+const getTokenByUserId = (sqlite: Database, userId: string) =>
+	sqlite
+		.query<DBColumns, Pick<InsertDBRow, '$userId'>>(
+			'SELECT * FROM tokens_table WHERE userId = $userId',
+		)
+		.get({ $userId: userId })
+
+/**
+ * Create a database for storing tokens
+ * @param db - The database instance
+ * @returns - An object with methods for interacting with the database
+ */
+export const createTokensDB = (db: Database) => {
+	db.run(`CREATE TABLE IF NOT EXISTS tokens_table (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            userId TEXT UNIQUE,
+            locationId TEXT,
+            access_token TEXT UNIQUE,
+            refresh_token TEXT UNIQUE,
+            expiresAt INTEGER
+        )
+        `)
+
+	return {
+		getAccessToken: () => getAccessToken(db),
+		saveTokenResponse: (dbRow: DBColumns) => saveTokenResponse(db, dbRow),
+		getTokenByUserId: (userId: string) => getTokenByUserId(db, userId),
+	}
+}
