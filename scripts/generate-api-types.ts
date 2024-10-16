fe05ed707dfe1f0bd45edae4c1a2a51ff49c00cf
gnostic <64601257+nynxa@users.noreply.github.com>
2024-10-16 00:09:47 -0400

    chore: no more barrel files they make things too slow

--- scripts/generate-api-types.ts
+++ scripts/generate-api-types.ts
@@ -1,11 +1,33 @@
 import path from 'node:path'
 import kleur from 'kleur'
-import openapiTS, { astToString } from 'openapi-typescript'
+import openapiTS, {
+	astToString,
+	type OpenAPI3,
+	type OperationObject,
+	type ParameterObject,
+} from 'openapi-typescript'
 import { OPENAPI_TYPES_V2_DIR } from '../src/lib/constants'
+
+class TempJson implements AsyncDisposable {
+	path: string
+	constructor(path: string, data?: string) {
+		this.path = path
+		if (data) {
+			this.write(data)
+		}
+	}
+	async [Symbol.asyncDispose]() {
+		await Bun.$`rm -f ${this.path}`
+	}
+	async write(data: string) {
+		await Bun.write(this.path, data)
+	}
+}
 
 const TEMP_DIR = 'temp-schema-types'
 
 if (import.meta.main) {
+	// dont run this unless the api changes.
 	await createV1Types()
 	await generateOpenApiTypes()
 }
@@ -13,19 +35,63 @@ if (import.meta.main) {
 async function createV1Types() {
 	const postmanJson = Bun.resolveSync('schemas/v1/postman.json', process.cwd())
 	// convert postman json to openapi json using postman-to-openapi
-	const openapiYaml = Bun.pathToFileURL('schemas/v1/openapi.yaml')
-	console.log('openapiYaml', openapiYaml.pathname)
-	// change the openapi version to 3.1.0 then write to file
-	const yaml =
-		await Bun.$`bunx --bun postman-to-openapi ${postmanJson} | sed 's/3.0.0/3.1.0/'`.blob()
-	await Bun.write(openapiYaml.pathname, yaml)
+	// const openapiYaml = Bun.pathToFileURL('schemas/v1/openapi.yaml')
+	// create temp json options file
+	const opts = {
+		servers: [
+			{
+				url: 'https://rest.gohighlevel.com',
+				description: 'v1 API base URL',
+			},
+		],
+		auth: {
+			bearer: {
+				type: 'http',
+				scheme: 'bearer',
+				name: 'Authorization',
+			},
+		},
+		outputFormat: 'json',
+	}
+
+	const requiredParams: ParameterObject = {
+		name: 'Authorization',
+		in: 'header',
+		description: 'Access Token',
+		required: true,
+		schema: {
+			type: 'string',
+			example: 'Bearer 9c48df2694a849b6089f9d0d3513efe',
+		},
+	}
+
+	const optionsJson = new TempJson('temp-v1-json.json', JSON.stringify(opts))
+	const openapiJsonFile = 'schemas/v1/openapi.json'
+
+	await Bun.$`bunx --bun postman-to-openapi ${postmanJson} -f ${openapiJsonFile} -o ${optionsJson.path}`
+	const openapiJson: OpenAPI3 = await Bun.file(openapiJsonFile).json()
+	// add the required headers to each path's parameters object
+	const paths = openapiJson.paths
+	if (!paths) {
+		throw new Error('No paths found in openapi data')
+	}
+
+	for (const path of objectKeys(paths)) {
+		for (const method of objectKeys(paths[path]!)) {
+			const methodObj = paths[path]![method]! as OperationObject
+			methodObj.parameters?.push(requiredParams)
+		}
+	}
+
+	await Bun.write(openapiJsonFile, JSON.stringify(openapiJson))
 	console.log(
 		kleur.green('Successfully converted postman json to openapi json'),
 	)
 	// convert openapi json to typescript types using openapi-typescript
-	const openapiTypes = await openapiTS(openapiYaml, {
+	const openapiTypes = await openapiTS(openapiJson, {
 		version: 3.0,
 		defaultNonNullable: true,
+		alphabetize: true,
 		exportType: true,
 	})
 	const data = astToString(openapiTypes)
@@ -45,35 +111,6 @@ async function getV2OpenApiFiles() {
 		throw new Error('No files found in schemas')
 	}
 	return files.map((file) => Bun.pathToFileURL(file))
-}
-
-/**
- * Generate an index file for the schemas.
- */
-async function generateIndexFile(directory: string) {
-	const glob = new Bun.Glob(`${directory}/*.ts`)
-	const schemaFiles = await Array.fromAsync(glob.scan())
-
-	let importData = ''
-	const exportData: {
-		fileName: string
-		upperCamelCaseExportName: string
-	}[] = []
-
-	for (const file of schemaFiles) {
-		if (!file.endsWith('.ts') || file === 'index.ts') {
-			continue
-		}
-		const fileName = path.basename(file, '.ts')
-		const upperCamelCaseExportName = fileName
-			.split(/[-\s]/)
-			.map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-			.join('')
-		importData += `export * as ${upperCamelCaseExportName} from './${fileName}'\n`
-		exportData.push({ fileName, upperCamelCaseExportName })
-	}
-
-	await Bun.write(path.join(directory, 'index.ts'), importData)
 }
 
 async function createOpenApiTypesFile(file: URL) {
@@ -113,10 +150,6 @@ export async function generateOpenApiTypes() {
 
 		console.log(kleur.green('Successfully created all types'))
 
-		// Generate index file in temp directory
-		await generateIndexFile(TEMP_DIR)
-		console.log(kleur.green('Created index file'))
-
 		// sync files to final directory
 		await Bun.$`mkdir -p ${OPENAPI_TYPES_V2_DIR}`
 		await Bun.$`rsync -r ${TEMP_DIR}/ ${OPENAPI_TYPES_V2_DIR}/`
@@ -129,4 +162,8 @@ export async function generateOpenApiTypes() {
 		// Clean up temp directory
 		await Bun.$`rm -rf ${TEMP_DIR}`
 	}
+}
+
+function objectKeys<T extends object>(obj: T): (keyof T)[] {
+	return Object.keys(obj) as (keyof T)[]
 }
